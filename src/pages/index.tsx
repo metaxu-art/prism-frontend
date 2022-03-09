e54f2788c9c3eb4ce861afb2be09819dc9017e62
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-03-09 14:52:40 +0100

    setup tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
 // import Head from 'next/head';
 // import Image from 'next/image';
 
 const Home: NextPage = () => {
-	return <div> Hello Wolrd</div>;
+	useEffect(() => {
+		fetch('/hello').then((data) => {
+			console.log('data', data);
+		});
+	}, []);
+	return <div className="bg-red-200">tailwind is working</div>;
 };
 
 export default Home;
