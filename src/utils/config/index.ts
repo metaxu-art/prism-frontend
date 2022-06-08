e70a86a9f0ee1e81f0c7ebcfb1f04ccaa3bae809
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-06-08 16:59:48 +0200

    changed contract addresses

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -2,10 +2,8 @@ import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
 
 const config = {
-	// projectContractAddress: '0x778834A9f6D139e45aD559c8A5f1B8c2AFfE3321', // mumbai testnet
-	// tokensContractAddress: '0x67bce7F38C0Ee04C7C986A2Acc46697004229861', // mumbai testnet
-	projectContractAddress: '0xBE921c99C563141de9802e7888906BFd7b3514E4', // polygon
-	tokensContractAddress: '0x478a2F0fc28C33F9a6cDC464C3453F7f290deE90', // polygon
+	projectContractAddress: '0xe957a12c5546808E8E071EC5b910f0EF59554D99', // mumbai
+	tokensContractAddress: '0x5CfAc860BE6Ad5e770e7E5D9fa31Fb255Ec6b4b4', // mumbai
 	pinata: {
 		apiKey: process.env.PINATA_API_KEY || '',
 		secretKey: process.env.PINATA_SECRET_KEY || '',
