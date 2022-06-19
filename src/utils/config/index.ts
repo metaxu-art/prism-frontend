d83245f7782014597827a4ed1347f3dcd44fb6fb
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2022-06-19 12:57:27 +0200

    changed tokens abi + contract addresses

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -2,8 +2,8 @@ import { config as dotenvConfig } from 'dotenv';
 dotenvConfig();
 
 const config = {
-	projectContractAddress: '0xe957a12c5546808E8E071EC5b910f0EF59554D99', // mumbai
-	tokensContractAddress: '0x5CfAc860BE6Ad5e770e7E5D9fa31Fb255Ec6b4b4', // mumbai
+	projectContractAddress: '0x2E9563618F7cC7B4bef7784750cb8616a8Cb8D32', // mumbai
+	tokensContractAddress: '0x3455371c6A400686024E8f5cA8Fc59dca51A1666', // mumbai
 	pinata: {
 		apiKey: process.env.PINATA_API_KEY || '',
 		secretKey: process.env.PINATA_SECRET_KEY || '',
